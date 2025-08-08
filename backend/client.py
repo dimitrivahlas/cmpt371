e791b67c666e5910c709e8853393716078e08b96
unknown <sdongng@gmail.com>
2025-08-07 22:45:53 -0700

    ip info for game

--- backend/client.py
+++ backend/client.py
@@ -1,105 +1,91 @@
-#create a TCP/IP socket for a client
+
 import queue
 import socket
 import threading
 import subprocess, os, time
 
+
 def run_client():
-    #define server address and port to connect to
+    """
+    localhost client for echoing
+    """
+    # define server address and port to connect to
     host = 'localhost'
     port = 50558
 
-    #create tcp socket
+    # create tcp socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #connect to sever
+    # connect to sever
     s.connect((host, port))
 
-    #message we send to server
+    # message we send to server
     msg = "hello from client"
 
     while True:
-        #send message to sever encoded as bytes
+        # send message to sever encoded as bytes
         s.sendall(msg.encode('ascii'))
 
-        #recieve a response from server up to 1024 bytes
+        # recieve a response from server up to 1024 bytes
         data = s.recv(1024)
 
-
-
-        #decode and print server
+        # decode and print server
         print('Received from server:', data.decode('ascii'))
 
         ans = input('Do you want to continue (y/n): ')
         if ans.lower() != 'y':
             break
 
-    #close socket connection
+    # close socket connection
     s.close()
+
 
 def run_client2(ip, portno):
     """
     Run client whose connection will not be localhost
     """
 
-    #define server address and port to connect to
+    # define server address and port to connect to
     host = 'localhost'
     if ip is not None:
         host = ip
     port = portno
 
-    #create tcp socket
+    # create tcp socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #connect to sever
+    # connect to sever
     s.connect((host, port))
 
-    # #create queue for handling messages
-    # msg_queue = queue.Queue()
-    #
-    # def recv_loop():
-    #     try:
-    #         while True:
-    #             data = s.recv(1024)
-    #             if not data:
-    #                 break
-    #             msg_queue.put(data.decode('ascii'))
-    #     finally:
-    #         s.close()
-    #         msg_queue.put(None)
-    #
-    # threading.Thread(target=recv_loop, daemon=True).start()
-    #
-    # #
-    # s.setblocking(True)
-
-    #message we send to server
+    # message we send to server
     msg = "hello from client"
 
     while True:
-        #send message to sever encoded as bytes
+        # send message to sever encoded as bytes
         s.send(msg.encode('ascii'))
 
-        #recieve a response from server up to 1024 bytes
-        data = s.recv(1024)
+        # receive a response from server up to 1024 bytes
+        data = s.recv(1024).decode("ascii")
+        parts = data.split("|")
 
-        if data.decode('ascii') == "server: starting":
+        if parts[0] == "server_start":
             try:
+                host = parts[1]
+                port = parts[2]
+                print(f"Server is starting at {host}:{port}")
+
                 game_path = os.path.join("..", "frontend", "gui", "interface.py")
-                subprocess.Popen(["python", game_path])
+
+                #Open game
+                subprocess.Popen(["python", game_path, host, port])
                 time.sleep(5)
                 break
             except Exception as e:
                 print("Failed to start game:", e)
-        #decode and print server
-        print('Received from server:', data.decode('ascii'))
 
-        # ans = input('Do you want to continue (y/n): ')
-        # if ans.lower() != 'y':
-        #     break
-
-    #close socket connection
+    # close socket connection
     s.close()
-    
+
+
 if __name__ == '__main__':
     run_client()
